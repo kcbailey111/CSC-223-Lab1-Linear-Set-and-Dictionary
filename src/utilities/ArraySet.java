ced53cb2f755dfc40003dcf143abe76dab67b1e7
brodeeC <brodeeclontz@gmail.com>
2024-01-13 14:55:05 -0500

    Adding methods
    I've added all the methods to ArraySet, 4 still need code.
    ArraySetTest has a unit test that works.
    I've only added methods into the other class, I haven't made any tests or other changes so far.

--- src/utilities/ArraySet.java
+++ src/utilities/ArraySet.java
@@ -16,6 +16,101 @@ public class ArraySet<E> implements List<E>, Set<E>
 	{
 		_list = new ArrayList<E>();
 	}
+	
+	public ArraySet(Collection <? extends E> c) {
+		_list = new ArrayList<E>(c);
+	}
+	@Override
+	public int size() {return _list.size();}
+
+	@Override
+	public boolean isEmpty() {return _list.isEmpty();}
+
+	@Override
+	public boolean contains(Object o) {return _list.contains(o);}
+		
+
+	@Override
+	public Iterator<E> iterator() {return _list.iterator();}
+
+	@Override
+	public Object[] toArray() {return _list.toArray();}
+
+	@Override
+	public <T> T[] toArray(T[] a) {return _list.toArray(a);}
+
+	@Override
+	public boolean add(E e) {
+		//TODO: Code add in, can't delegate its function like the others
+		return false;
+	}
+
+	@Override
+	public boolean remove(Object o) {return _list.remove(o);}
+		
+
+	@Override
+	public boolean containsAll(Collection<?> c) {return _list.containsAll(c);}
+		
+
+	@Override
+	public boolean addAll(Collection<? extends E> c) {
+		//TODO: Code addAll in, can't delegate its function like the others
+		return false;
+	}
+
+	@Override
+	public boolean addAll(int index, Collection<? extends E> c) {
+		//TODO: Code addAll in, can't delegate its function like the others
+		return false;
+	}
+		
+
+	@Override
+	public boolean removeAll(Collection<?> c) {
+		_list.clear();
+		if (_list.isEmpty()) return true;
+		return false;
+	}
+
+	@Override
+	public boolean retainAll(Collection<?> c) {
+		//TODO: Code retainAll in, can't delegate its function like the others
+		return false;
+	}
+
+	@Override
+	public void clear() {_list.clear();}
+
+	@Override
+	public E get(int index) {return _list.get(index);}
+
+	@Override
+	public E set(int index, E element) {return _list.set(index, element);}
+
+	@Override
+	public void add(int index, E element) {_list.add(index, element);}
+
+	@Override
+	public E remove(int index) {return _list.remove(index);}
+
+	@Override
+	public int indexOf(Object o) {return _list.indexOf(o);}
+
+	@Override
+	public int lastIndexOf(Object o) {return _list.lastIndexOf(o);}
+
+	@Override
+	public ListIterator<E> listIterator() {return _list.listIterator();}
+
+	@Override
+	public ListIterator<E> listIterator(int index) {return _list.listIterator(index);}
+
+	@Override
+	public List<E> subList(int fromIndex, int toIndex) {return _list.subList(fromIndex, toIndex);}
+
+	@Override
+	public Spliterator<E> spliterator() {return _list.spliterator();}
 
 
 }
--- test/utilities/ArraySetTest.java
+++ test/utilities/ArraySetTest.java
@@ -1,5 +1,9 @@
 package utilities;
 
+import static org.junit.Assert.assertTrue;
+
+import java.util.ArrayList;
+
 import org.junit.jupiter.api.Test;
 
 class ArraySetTest
@@ -9,6 +13,14 @@ class ArraySetTest
 	@Test
 	void testArraySetCollectionOfE()
 	{
+		ArrayList<String> colOfStrings = new ArrayList<String>();
+		colOfStrings.add("hello");
+		colOfStrings.add("world");
+		colOfStrings.add("tsunami");
+		ArraySet<String> _list = new ArraySet<String>(colOfStrings);
+		assertTrue(_list.contains("hello"));
+		assertTrue(_list.contains("world"));
+		assertTrue(_list.contains("tsunami"));
 	}
 
 	@Test
