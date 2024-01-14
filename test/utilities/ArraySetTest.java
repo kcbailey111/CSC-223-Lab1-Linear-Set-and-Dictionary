12c8794f5ce40abb739b8106fdd8fa7a677768d9
brodeeC <brodeeclontz@gmail.com>
2024-01-14 10:49:11 -0500

    testAddE()
    Wrote the test for add
    Commented the three tests I've written

--- test/utilities/ArraySetTest.java
+++ test/utilities/ArraySetTest.java
@@ -34,14 +34,18 @@ class ArraySetTest
 		//Making a new list with the collection and testing that it only adds one unique element
 		ArraySet<String> _newList = new ArraySet<String>(colOfStrings);
 		assertTrue(_newList.size()==1);
-		
-		
-		
 	}
 
 	@Test
 	void testAddE()
 	{
+		//Creating a loop to add 100 of the same element
+		ArraySet<String> _list = new ArraySet<String>();
+		for (int i = 0; i < 100; i ++) {
+			_list.add("hello");
+		}
+		//Only one element should ever be added since all are the same
+		assertTrue(_list.size()==1);
 	}
 
 	@Test
@@ -57,14 +61,18 @@ class ArraySetTest
 	@Test
 	void testRemoveAll()
 	{
+		//Creating a collection
 		ArrayList<String> colOfStrings = new ArrayList<String>();
 		colOfStrings.add("hello");
 		colOfStrings.add("world");
 		
+		//Creating a list with 3 elements, 2 are in the collection
 		ArraySet<String> _list = new ArraySet<String>();
 		_list.add("hello");
 		_list.add("world");
 		_list.add("tsunami");
+		
+		//Testing to see that the 2 elements from the collection were removed
 		_list.removeAll(colOfStrings);
 		assertTrue(_list.get(0).equals("tsunami"));
 	}
