837e4d1386ce36a4fc91a6c59ca75cb5a526867d
brodeeC <brodeeclontz@gmail.com>
2024-01-16 14:42:42 -0500

    Test changes
    ALmost done

--- test/utilities/ArraySetTest.java
+++ test/utilities/ArraySetTest.java
@@ -7,9 +7,11 @@
 
 package utilities;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 
 import org.junit.jupiter.api.Test;
 
@@ -21,26 +23,20 @@ class ArraySetTest
 	void testArraySetCollectionOfE()
 	{
 		//Creating a collection to test the parameterized constructor
-		ArrayList<String> colOfStrings = new ArrayList<String>();
-		colOfStrings.add("hello");
-		colOfStrings.add("world");
-		colOfStrings.add("tsunami");
+		ArrayList<String> colOfStrings = new ArrayList<String>(Arrays.asList("hello", "world", "tsunami"));
 
 		//Testing the constructor with unique elements
 		ArraySet<String> _list = new ArraySet<String>(colOfStrings);
-		assertTrue(_list.contains("hello"));
-		assertTrue(_list.contains("world"));
-		assertTrue(_list.contains("tsunami"));
+		assertTrue("Doesn't contain hello", _list.contains("hello"));
+		assertTrue("Doesn't contain world",_list.contains("world"));
+		assertTrue("Doesn't contain tsunami",_list.contains("tsunami"));
 
 		//Clearing the collection and putting new non-unique values in
-		colOfStrings.clear();
-		colOfStrings.add("hello");
-		colOfStrings.add("hello");
-		colOfStrings.add("hello");
+		colOfStrings = new ArrayList<String>(Arrays.asList("hello", "hello", "hello"));
 
 		//Making a new list with the collection and testing that it only adds one unique element
 		ArraySet<String> _newList = new ArraySet<String>(colOfStrings);
-		assertTrue(_newList.size()==1);
+		assertEquals(_newList.size(), 1);
 	}
 
 	@Test
@@ -52,51 +48,38 @@ class ArraySetTest
 			_list.add("hello");
 		}
 		//Only one element should ever be added since all are the same
-		assertTrue(_list.size()==1);
+		assertEquals(_list.size(), 1);
 	}
 
 	@Test
 	void testAddAllCollectionOfQextendsE()
 	{
 		//Creating and collection to addAll to the list
-		ArrayList<String> colOfStrings = new ArrayList<String>();
-		colOfStrings.add("hello");
-		colOfStrings.add("world");
-		colOfStrings.add("tsunami");
+		ArrayList<String> colOfStrings = new ArrayList<String>(Arrays.asList("hello", "world", "tsunami"));
 
 		//Creating a list with one element from the collection and one not
-		ArraySet<String> _list = new ArraySet<String>();
-		_list.add("first");
-		_list.add("tsunami");
+		ArraySet<String> _list = new ArraySet<String>(Arrays.asList("first", "tsunami"));
 		_list.addAll(colOfStrings);
 
 		//Shouldn't add an additional "tsunami" so size should be 4
-		assertTrue(_list.size()==4);
+		assertEquals(_list.size(), 4);
 	}
 
 	@Test
 	void testRetainAll()
 	{
-		//Creating and collection to addAll to the list
-		ArrayList<String> colOfStrings = new ArrayList<String>();
-		colOfStrings.add("hello");
-		colOfStrings.add("world");
-		colOfStrings.add("tsunami");
+		ArrayList<String> colOfStrings = new ArrayList<String>(Arrays.asList("hello", "world", "tsunami"));
 
 		//Creating a list with one element from the collection and one not
-		ArraySet<String> _list = new ArraySet<String>();
-		_list.add("first");
-		_list.add("tsunami");
+		ArraySet<String> _list = new ArraySet<String>(Arrays.asList("first", "tsunami"));
 		_list.retainAll(colOfStrings);
 
-		//Shouldn't add an additional "tsunami" so size should be 4
+		//Should only retain the 3 elements in colOfStrings
 		assertTrue(_list.size()==3);
+		assertEquals(_list.subList(0,3), Arrays.asList("hello", "world", "tsunami"));
 		
 		//Adding the same element 3 times to the collection
-		colOfStrings.clear();
-		colOfStrings.add("hello");
-		colOfStrings.add("hello");
-		colOfStrings.add("hello");
+		colOfStrings = new ArrayList<String>(Arrays.asList("hello", "hello", "hello"));
 		
 		//It should only retain one unique element so size should be 1
 		_list.retainAll(colOfStrings);
@@ -107,43 +90,30 @@ class ArraySetTest
 	void testRemoveAll()
 	{
 		//Creating a collection
-		ArrayList<String> colOfStrings = new ArrayList<String>();
-		colOfStrings.add("hello");
-		colOfStrings.add("world");
+		ArrayList<String> colOfStrings = new ArrayList<String>(Arrays.asList("hello", "world"));
 
 		//Creating a list with 3 elements, 2 are in the collection
-		ArraySet<String> _list = new ArraySet<String>();
-		_list.add("hello");
-		_list.add("world");
-		_list.add("tsunami");
+		ArraySet<String> _list = new ArraySet<String>(Arrays.asList("hello", "world", "tsunami"));
 
 		//Testing to see that the 2 elements from the collection were removed
 		_list.removeAll(colOfStrings);
-		assertTrue(_list.get(0).equals("tsunami"));
+		assertEquals(_list.get(0), "tsunami");
 	}
 
 	@Test
 	void testAddAllIntCollectionOfQextendsE()
 	{
 		//Creating and collection to addAll to the list
-		ArrayList<String> colOfStrings = new ArrayList<String>();
-		colOfStrings.add("hello");
-		colOfStrings.add("world");
-		colOfStrings.add("tsunami");
+		ArrayList<String> colOfStrings = new ArrayList<String>(Arrays.asList("hello", "world", "tsunami"));
 
 		//Creating a list with one element from the collection and one not
-		ArraySet<String> _list = new ArraySet<String>();
-		_list.add("first");
-		_list.add("tsunami");
+		ArraySet<String> _list = new ArraySet<String>(Arrays.asList("first", "tsunami"));
 		_list.addAll(1, colOfStrings);
 
 		//Shouldn't add an additional "tsunami" so size should be 4
-		assertTrue(_list.size()==4);
+		assertEquals(_list.size(), 4);
 
 		//Testing to make sure the elements were inserted in the right place
-		assertTrue(_list.get(0).equals("first"));
-		assertTrue(_list.get(1).equals("hello"));
-		assertTrue(_list.get(2).equals("world"));
-		assertTrue(_list.get(3).equals("tsunami"));
+		assertEquals(_list.subList(0, 4), Arrays.asList("first", "hello", "world", "tsunami"));
 	}
 }
